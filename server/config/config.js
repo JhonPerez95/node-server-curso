6abe26f00eba77de8c5057f9c6b7eee35e639e6d
JhonPerez95 <jhonperez.j95@gmail.com>
2020-04-07 21:34:13 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlBD;
 if (process.env.NODE_ENV === "dev") {
   urlBD = "mongodb://localhost:27017/cafe";
 } else {
-  urlBD = "process.env.MONGO_URI";
+  urlBD = process.env.MONGO_URI;
 }
 process.env.URLDB = urlBD;
